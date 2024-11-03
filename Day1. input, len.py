5cafef357dc28bb7e77344296575a19fe76b1480
KwangBeom Kwon <63405611+TerryKwon115@users.noreply.github.com>
2024-11-03 20:11:27 +1000

    Add files via upload

---
+++ Day1. input, len.py
@@ -0,0 +1,14 @@
+# input
+
+print("Hello, " + input("What is your name? ") + "!")
+
+name = input("What is your name? ")
+print(f"Nice to meet you, {name}.")
+
+# len
+
+print(len(input("What is your name? ")))
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
