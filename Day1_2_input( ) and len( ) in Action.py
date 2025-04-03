a275cfb2cf2f90cb4795b18de693e35004cde01a
KwangBeom Kwon <63405611+TerryKwon115@users.noreply.github.com>
2025-04-04 01:47:08 +1000

    Update Day1_2_input( ) and len( ) in Action.py

--- Day1_2_input( ) and len( ) in Action.py
+++ Day1_2_input( ) and len( ) in Action.py
@@ -1,11 +1,11 @@
-# input
+# input()
 
 print("Hello, " + input("What is your name? ") + "!")
 
 name = input("What is your name? ")
 print(f"Nice to meet you, {name}.")
 
-# len
+# len()
 
 print(len(input("What is your name? ")))
 
